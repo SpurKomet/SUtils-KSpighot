3dd6310697637840131fc481bb11488fdebb090d
SpurKomet <unconfigured@null.spigotmc.org>
2021-10-11 23:27:35 +0200

    color

--- src/main/kotlin/net/spurkomet/sutils/listener/JoinListener.kt
+++ src/main/kotlin/net/spurkomet/sutils/listener/JoinListener.kt
@@ -8,8 +8,8 @@ import org.bukkit.event.player.PlayerJoinEvent
 fun joinListener(){
     listen<PlayerJoinEvent>{
         it.joinMessage = "${col("dark_gray")}[${col("dark_green")}+${col("dark_gray")}] ${col(if (it.player.isOp)"aqua" else "white")}${it.player.name}"
-        /*it.player.title( "${col("yellow")}Willkommen! ${if(it.player.isOp) col("dark_red") else col("gray")}${it.player.name}",
-            "${col ("dark_blue")}SUtils Server | powered by ${col("aqua")}Kot${col("white")}lin",
+        /*it.player.title( "${col("yellow")}Willkommen! ${if(it.player.isOp) col("aqua") else col("white")}${it.player.name}",
+            "${col ("dark_aqua")}SUtils Server | powered by ${col("aqua")}Kot${col("white")}lin",
             4, 40, 10)*/
     }
 }
